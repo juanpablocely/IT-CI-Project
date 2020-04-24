0a13a55e06a1fef91ac048722abfcec62e5536c4
Jhon Rayo <RayoJhon99@gmail.com>
2020-04-24 12:24:38 -0500

    setup basic react app

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" />
+        <h2 className='Text'>
+          Taller de Infraestructura de TI
+        </h2>
+        <p className='Text'> Jhon Rayo </p>
+        <p className='Text'> Juan Pablo </p>
+        <p className='Text'> Carlos Parra </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
